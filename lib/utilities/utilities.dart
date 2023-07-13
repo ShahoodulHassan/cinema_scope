c4b94e9f33712fe0095f9034203adef620fa2ab8
Shahood <shahood@gmail.com>
2023-07-13 16:15:08 +0500

    Removed methods from mixing and moved these to an extension on Color class

--- lib/utilities/utilities.dart
+++ lib/utilities/utilities.dart
@@ -84,49 +84,5 @@ mixin Utilities {
   String getYearStringFromDate(String? date) =>
       '${getYearFromDate(date) ?? ''}';
 
-  // ranges from 0.0 to 1.0
-  Color darken(Color color, [double amount = 0.1]) {
-    assert(amount >= 0 && amount <= 1);
-
-    final hsl = HSLColor.fromColor(color);
-    final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
-
-    return hslDark.toColor();
-  }
-
-  Color lighten(Color color, [double amount = 0.1]) {
-    assert(amount >= 0 && amount <= 1);
-
-    final hsl = HSLColor.fromColor(color);
-    final hslLight = hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
-
-    return hslLight.toColor();
-  }
-
-  /// Darken a color by [percent] amount (100 = black)
-// ........................................................
-  Color darken2(Color c, [int percent = 10]) {
-    assert(1 <= percent && percent <= 100);
-    var f = 1 - percent / 100;
-    return Color.fromARGB(
-        c.alpha,
-        (c.red * f).round(),
-        (c.green  * f).round(),
-        (c.blue * f).round()
-    );
-  }
-
-  /// Lighten a color by [percent] amount (100 = white)
-// ........................................................
-  Color lighten2(Color c, [int percent = 10]) {
-    assert(1 <= percent && percent <= 100);
-    var p = percent / 100;
-    return Color.fromARGB(
-        c.alpha,
-        c.red + ((255 - c.red) * p).round(),
-        c.green + ((255 - c.green) * p).round(),
-        c.blue + ((255 - c.blue) * p).round()
-    );
-  }
 
 }
